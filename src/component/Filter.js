211896f99d71002eaa587ea36388b664d543cc16
Amit Awasthi <as.9602.amit@gmail.com>
2024-05-06 06:28:25 +0530

    providing seperate filter data

--- src/component/Filter.js
+++ src/component/Filter.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Multiselect } from "multiselect-react-dropdown";
-
+import {RoleOptions,NoOfEmpOptions,Experience,JobType,MinPaySalary} from './FilterData';
 import JobPages from './JobPages';
 import './filter.css';
 const Filter = (props) => {
@@ -8,48 +8,7 @@ const Filter = (props) => {
     const [filterJobs,setFilterJob] = useState();
 const [selectedFlavors, setSelectedFlavors] = useState([]);
 const selectedFilter = [];
-  const RoleOptions = [
-    { value: "Role", key: "backend" },
-    { value: "Role", key: "frontend" },
-    { value: "Role", key: "android" },
-    { value: "Role", key: "ios" },
-    { value: "Role", key: "flutter" },
-    { value: "Role", key: "fullstack" }
-  ];
-  const NoOfEmpOptions = [
-    { value: "1-10", key: "1-10" },
-    { value: "11-20", key: "21-50" },
-    { value: "51-100", key: "51-100" },
-    { value: "101-200", key: "101-200" },
-    { value: "201-500", key: "201-500" },
-    { value: "500+", key: "500+" }
-  ];
-  const Experience = [
-    { value: "maxExp", key: "1" },
-    { value: "maxExp", key: "2" },
-    { value: "maxExp", key: "3" },
-    { value: "maxExp", key: "4" },
-    { value: "maxExp", key: "5" },
-    { value: "maxExp", key: "6" },
-    { value: "maxExp", key: "7" },
-    { value: "maxExp", key: "8" },
-    { value: "maxExp", key: "9" },
-    { value: "maxExp", key: "10" },
-  ];
-  const JobType = [
-    { value: "Remote", key: "Remote" },
-    { value: "Hybrid", key: "Hybrid" },
-    { value: "In-Office", key: "In-Office" },
-  ];
-  const MinPaySalary = [
-    { value: "minJdSalary", key: 10 },
-    { value: "minJdSalary", key: 20 },
-    { value: "minJdSalary", key: 30 },
-    { value: "minJdSalary", key: 40 },
-    { value: "minJdSalary", key: 50 },
-    { value: "minJdSalary", key: 60 },
-    { value: "minJdSalary", key: 70 },
-  ];
+  
   const onSelect = (e) => {
     setSelectedFlavors(e);
     //console.log(e)    
@@ -62,7 +21,7 @@ const selectedFilter = [];
                         tempFilterJob.push(job);
                     }
                 }else if(flavors.value === 'maxExp'){
-                    if(job.maxExp == flavors.key){
+                    if(job.maxExp >= flavors.key){
                         tempFilterJob.push(job);
                     }
                 }else if(flavors.value === 'minJdSalary'){
@@ -129,7 +88,8 @@ const selectedFilter = [];
         onSelect={onSelect}      
         options= {Experience}
         placeholder="Experience"
-        className='dropDown'
+        className='dropDown'        
+        singleSelect
         />
          <Multiselect
         displayValue="key"
---
+++ src/component/FilterData.js
@@ -0,0 +1,44 @@
+const RoleOptions = [
+    { value: "Role", key: "backend" },
+    { value: "Role", key: "frontend" },
+    { value: "Role", key: "android" },
+    { value: "Role", key: "ios" },
+    { value: "Role", key: "flutter" },
+    { value: "Role", key: "fullstack" }
+  ];
+  const NoOfEmpOptions = [
+    { value: "1-10", key: "1-10" },
+    { value: "11-20", key: "21-50" },
+    { value: "51-100", key: "51-100" },
+    { value: "101-200", key: "101-200" },
+    { value: "201-500", key: "201-500" },
+    { value: "500+", key: "500+" }
+  ];
+  const Experience = [
+    { value: "maxExp", key: 1 },
+    { value: "maxExp", key: 2 },
+    { value: "maxExp", key: 3 },
+    { value: "maxExp", key: 4 },
+    { value: "maxExp", key: 5 },
+    { value: "maxExp", key: 6 },
+    { value: "maxExp", key: 7 },
+    { value: "maxExp", key: 8 },
+    { value: "maxExp", key: 9 },
+    { value: "maxExp", key: 10 },
+  ];
+  const JobType = [
+    { value: "Remote", key: "Remote" },
+    { value: "Hybrid", key: "Hybrid" },
+    { value: "In-Office", key: "In-Office" },
+  ];
+  const MinPaySalary = [
+    { value: "minJdSalary", key: 10 },
+    { value: "minJdSalary", key: 20 },
+    { value: "minJdSalary", key: 30 },
+    { value: "minJdSalary", key: 40 },
+    { value: "minJdSalary", key: 50 },
+    { value: "minJdSalary", key: 60 },
+    { value: "minJdSalary", key: 70 },
+  ];
+
+  export {RoleOptions,NoOfEmpOptions,Experience,JobType,MinPaySalary};
